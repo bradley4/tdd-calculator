2d3cbaa030d20b1b19b71d43c7a9cb7ea9e80199
bradley4 <yesyesyes@hotmail.co.kr>
2022-01-26 13:59:08 +0900

    feat: 계산기 TDD 곱셈, 나눗셈 구현

--- src/main/java/study/Calculator.java
+++ src/main/java/study/Calculator.java
@@ -8,4 +8,12 @@ public class Calculator {
     public int minus(int input1, int input2) {
         return input1 - input2;
     }
+
+    public int multiply(int input1, int input2) {
+        return input1 * input2;
+    }
+
+    public int divide(int input1, int input2) {
+        return input1 / input2;
+    }
 }
--- src/test/java/study/CalculatorTest.java
+++ src/test/java/study/CalculatorTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CalculatorTest {
 
@@ -18,36 +19,63 @@ class CalculatorTest {
     @DisplayName("두 수를 더해서 합을 리턴한다")
     @Test
     void plus() {
-        int input1 = 3;
-        int input2 = 2;
-        assertThat(calculator.plus(input1, input2)).isEqualTo(5);
+        final int givenInput1 = 3;
+        final int givenInput2 = 2;
+
+        assertThat(calculator.plus(givenInput1, givenInput2)).isEqualTo(5);
     }
 
     @DisplayName("두 수를 빼서 차를 리턴한다")
     @Test
     void minus() {
         // given
-        int input1 = 5;
-        int input2 = 3;
+        final int givenInput1 = 5;
+        final int givenInput2 = 3;
 
         // when
-        int actual = calculator.minus(input1, input2);
+        final int actual = calculator.minus(givenInput1, givenInput2);
 
         // then
         assertThat(actual).isEqualTo(2);
     }
 
-    @DisplayName("두 수를 빼서 차를 리턴한다")
+    @DisplayName("두 수를 곱해서 곱을 리턴한다.")
+    @Test
+    void multiply() {
+        // given
+        final int givenInput1 = 5;
+        final int givenInput2 = 3;
+
+        // when
+        final int actual = calculator.multiply(givenInput1, givenInput2);
+
+        // then
+        assertThat(actual).isEqualTo(15);
+    }
+
+    @DisplayName("두 수를 나눠서 나눈 값을 리턴한다.")
     @Test
     void divide() {
         // given
-        int input1 = 5;
-        int input2 = 3;
+        final int givenInput1 = 4;
+        final int givenInput2 = 2;
 
         // when
-        int actual = calculator.minus(input1, input2);
+        final int actual = calculator.divide(givenInput1, givenInput2);
 
         // then
         assertThat(actual).isEqualTo(2);
+    }
+
+    @DisplayName("두 수 중 하나를 0으로 나누면 예외를 던진다.")
+    @Test
+    void divide_with_zero() {
+        // given
+        final int givenInput1 = 4;
+        final int givenInput2 = 0;
+
+        // when
+        assertThatThrownBy(() -> calculator.divide(givenInput1, givenInput2))
+                .isInstanceOf(ArithmeticException.class);
     }
 }
