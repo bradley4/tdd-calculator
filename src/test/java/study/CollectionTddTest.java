78c972e8db80a3336b324c4160b47c1230500505
kkoon9 <rndrnjs2003@gmail.com>
2022-01-29 16:46:26 +0900

    feat: 요구사항 [2] - ParameterizedTest
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/test/java/study/CollectionTddTest.java
+++ src/test/java/study/CollectionTddTest.java
@@ -4,12 +4,17 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class CollectionTddTest {
     private Set<Integer> numbers;
+
     @BeforeEach
     void setUp() {
         numbers = new HashSet<>();
@@ -32,6 +37,14 @@ public class CollectionTddTest {
         Assertions
                 .assertThat(result)
                 .isEqualTo(3);
+    }
+
+    @DisplayName("contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains_test_0(int number) {
+        // then
+        assertTrue(numbers.contains(number));
     }
 
 }
