fdcf251b830b9e29c4880d53113da8fa10ceeb81
kkoon9 <rndrnjs2003@gmail.com>
2022-01-29 21:24:23 +0900

    feat: 커스텀 구분자 테스트+ 구분자, 숫자 이외의 값을 전달하는 경우 RuntimeException 발생 테스트
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,10 +1,12 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class Calculator {
     int plus(String input) {
-        if(input.isEmpty()) {
+        if (input.isEmpty()) {
             return 0;
         }
         return 0; // 임시 return 문
@@ -12,12 +14,40 @@ public class Calculator {
 
     int[] split(String input) {
         String regex = ",|:";
+
+        if (isCustomRegex(input)) {
+            String[] splittingResults = input.split("\\n");
+            input = splittingResults[1];
+            regex = splittingResults[0].replaceAll("//", "");
+        }
+        checkException(input, regex);
         String[] inputSplitComma = input.split(regex);
         int[] result =
                 Stream.of(inputSplitComma)
                         .mapToInt(Integer::parseInt)
                         .toArray();
         return result;
+    }
+
+    boolean isCustomRegex(String input) {
+        String regex = "//.+\\n.+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        return matcher.find();
+
+    }
+
+    void checkException(String input, String regex) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(regex);
+        sb.append("|0-9");
+        String exceptionRegex = sb.toString();
+        Pattern pattern = Pattern.compile(exceptionRegex);
+        Matcher matcher = pattern.matcher(input);
+        if (!matcher.find()) {
+            throw new RuntimeException("구분자, 숫자 이외의 값(음수 포함)을 포함합니다.");
+        }
+
     }
 
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -33,4 +33,52 @@ public class CalculatorTest {
         // then
         Assertions.assertThat(result).containsExactly(1,2);
     }
+
+    @DisplayName("쉼표(,) 또는 콜론(:)을 제외한 커스텀 구분자를 지정할 수 있다")
+    @Test
+    void is_custom_separator_test() {
+        // given
+        Calculator calculator = new Calculator();
+        String input = "//;\n1;2";
+
+        // when
+        boolean result = calculator.isCustomRegex(input);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(true);
+    }
+
+    @DisplayName("쉼표(,) 또는 콜론(:) 혹은 커스텀 구분자로 분리된 배열을 리턴한다")
+    @Test
+    void split_comma_colon_test_contain_custom_separator() {
+        // given
+        Calculator calculator = new Calculator();
+        String input = "//;\n1;2";
+
+        // when
+        int[] result = calculator.split(input);
+
+        // then
+        Assertions.assertThat(result).containsExactly(1,2);
+    }
+
+    @DisplayName("구분자, 숫자 이외의 값(음수 포함)을 전달하는 경우 RuntimeException 발생하는지에 대한 테스트")
+    @Test
+    void exception_input_test() {
+        // given
+        Calculator calculator = new Calculator();
+        String input = "//;\n1,2";
+
+        // when
+
+        // then
+        Assertions.assertThatThrownBy(
+                () -> {
+                    calculator.split(input);
+                }
+        )
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("구분자, 숫자 이외의 값(음수 포함)을 포함합니다.");
+    }
+
 }
