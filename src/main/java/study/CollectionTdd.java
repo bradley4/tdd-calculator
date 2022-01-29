4a7a845cfe92d51fee6dbfd5977ab74e6eace4de
kkoon9 <rndrnjs2003@gmail.com>
2022-01-29 14:46:56 +0900

    feat: CollectionTest 요구사항 [1]
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

---
+++ src/main/java/study/CollectionTdd.java
@@ -0,0 +1,10 @@
+package study;
+
+import java.util.Set;
+
+public class CollectionTdd {
+
+    int size(Set<Integer> set) {
+        return set.size();
+    }
+}
---
+++ src/test/java/study/CollectionTddTest.java
@@ -0,0 +1,37 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CollectionTddTest {
+    private Set<Integer> numbers;
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("size() 메소드를 활용해 Set의 크기를 확인하는 테스트")
+    @Test
+    void split_test_0() {
+        // given
+        CollectionTdd collectionTdd = new CollectionTdd();
+
+        // when
+        int result = collectionTdd.size(numbers);
+
+        // then
+        Assertions
+                .assertThat(result)
+                .isEqualTo(3);
+    }
+
+}
