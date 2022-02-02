b6ce6b437ad3df00490371dedf470be6986e8f7d
kkoon9 <rndrnjs2003@gmail.com>
2022-02-02 21:31:24 +0900

    fix: 코드 리뷰 반영 [1]
    - null 값을 전달하면 NullPointerException을 던지는 테스트 코드
    - 띄어쓰기를 포함한 빈 값을 전달하면 0을 리턴하는 테스트 코드
    - 콜론 테스트하는 코드를 위해 ParameterizedTest로 변경
    - 음수 테스트하는 코드를 위해 ParameterizedTest로 변경
    
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -3,8 +3,10 @@ package calculator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-public class CalculatorTest {
+class CalculatorTest {
 
     @DisplayName("빈 값을 전달하면 0을 리턴한다")
     @Test
@@ -20,12 +22,32 @@ public class CalculatorTest {
         Assertions.assertThat(result).isEqualTo(0);
     }
 
-    @DisplayName("쉼표(,) 또는 콜론(:)을 구분자로 분리된 배열을 리턴한다")
+    @DisplayName("null 값을 전달하면 NullPointerException을 발생시킨다")
     @Test
-    void split_comma_colon_test() {
+    void is_null_test() {
         // given
         Calculator calculator = new Calculator();
-        String input = "1,2";
+        String input = "//;\n1,2";
+
+        // when
+
+        // then
+        Assertions.assertThatThrownBy(
+                () -> {
+                    calculator.split(input);
+                }
+        )
+                .isInstanceOf(NullPointerException.class)
+                .hasMessageContaining("Null을 전달해서는 안 됩니다.");
+
+    }
+
+    @DisplayName("쉼표(,)를 구분자로 분리된 배열을 리턴한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2", "1:2"})
+    void split_comma_colon_test(String input) {
+        // given
+        Calculator calculator = new Calculator();
 
         // when
         int[] result = calculator.split(input);
@@ -63,11 +85,11 @@ public class CalculatorTest {
     }
 
     @DisplayName("구분자, 숫자 이외의 값(음수 포함)을 전달하는 경우 RuntimeException 발생하는지에 대한 테스트")
-    @Test
-    void exception_input_test() {
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\n1,2", "//;\n-1,-2"})
+    void exception_outlier_input_test(String input) {
         // given
         Calculator calculator = new Calculator();
-        String input = "//;\n1,2";
 
         // when
 
