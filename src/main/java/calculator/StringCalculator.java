3cbbb6f072d44f3ccb55a84cf062537b0fee3f92
lbhsos <bohyunlee.dev@gmail.com>
2022-02-04 14:44:50 +0900

    feat: 문자계산기 요구사항1 테스트 코드 및 개발 완료

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,2 +1,19 @@
-package calculator;public class StringCalculator {
+package calculator;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class StringCalculator {
+
+    public int addNumberBySeparation(String givenString) {
+
+        String[] list = givenString.split(",|:");
+        int sum = 0;
+        for ( String stringNumber : list ) {
+            if (StringUtils.isEmpty(stringNumber))
+                continue;
+            sum += Integer.parseInt(stringNumber);
+        }
+
+        return sum;
+    }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,2 +1,29 @@
-package calculator;public class StringCalculatorTest {
+package calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class StringCalculatorTest {
+
+    private StringCalculator stringCalculator;
+
+    @BeforeEach
+    void setUp() {
+        stringCalculator = new StringCalculator();
+    }
+
+    @DisplayName("쉼표 또는 콜론을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환")
+    @Test
+    void addNumberBySeparation() {
+        //given
+        String givenString = "1,2:3:0,:1,2,3";
+
+        //when
+        int result = stringCalculator.addNumberBySeparation(givenString);
+        //then
+        assertThat(result).isEqualTo(12);
+    }
 }
