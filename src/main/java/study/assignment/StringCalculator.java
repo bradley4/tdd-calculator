190859213ed23d6585eb7c1d37c88b95ff7db8bf
Yo0oN <4855chl@gmail.com>
2022-07-17 17:06:53 +0900

    REFACTOR: Replace magic number and magic literal with symbolic constant
    매직 넘버와 매직 리터럴 제거, 상수로 선언

--- src/main/java/study/assignment/StringCalculator.java
+++ src/main/java/study/assignment/StringCalculator.java
@@ -1,17 +1,27 @@
 package study.assignment;
 
 public class StringCalculator {
+    /*
+        매직 넘버 : 상수(static final)로 선언하지 않은 숫자
+        매직 리터럴 : 상수(static final)로 선언하지 않은 문자열
+        숫자나 문자를 선언하지 않고 코드 상에서 그대로 사용하게되면 읽는사람은 그것이 무엇을 의미하는지 알 수 없다.
+        상수로 선언하여 이름을 주어 어떤 역할을 하는지 알 수 있게 하자.
+     */
+    private static final String COMMA = ",";
+    private static final String COLON = ":";
+    private static final String LEFT_DELIMITER = "//";
+    private static final String RIGHT_DELIMITER = "\n";
 
     public int sumString(String input) {
         if (input == null || "".equals(input)) {
             return 0;
         }
-        if (input.contains("//") && input.contains("\n")) {
+        if (input.contains(LEFT_DELIMITER) && input.contains(RIGHT_DELIMITER)) {
             return sumStringWithCustomDelim(input);
         }
 
-        input = input.replaceAll(":", ",");
-        String[] commaSplitArray = input.split(",");
+        input = input.replaceAll(COLON, COMMA);
+        String[] commaSplitArray = input.split(COMMA);
         int result = 0;
         for (String splitInput : commaSplitArray) {
             checkInput(splitInput);
@@ -34,16 +44,16 @@ public class StringCalculator {
     }
 
     private String findCustomDelim(String input) {
-        int start = input.indexOf("//");
-        int end = input.indexOf("\n");
+        int start = input.indexOf(LEFT_DELIMITER) + LEFT_DELIMITER.length();
+        int end = input.indexOf(RIGHT_DELIMITER);
 
-        return input.substring(start + 2, end);
+        return input.substring(start, end);
     }
 
     private String findInput(String input) {
-        int start = input.indexOf("\n");
+        int start = input.indexOf(RIGHT_DELIMITER) + RIGHT_DELIMITER.length();
 
-        return input.substring(start + 1, input.length());
+        return input.substring(start);
     }
 
     private void checkInput(String splitInput){
